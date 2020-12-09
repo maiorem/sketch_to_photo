ea69e3817ad30d31206a319b2edcf77824595e05
Seyoung Hong <maiorem00@gmail.com>
2020-12-09 20:53:06 +0900

    flask

--- Project/code/load_model.py
+++ Project/code/load_model.py
@@ -5,17 +5,24 @@ from tensorflow.keras.preprocessing.image import load_img
 from numpy import load
 from numpy import expand_dims
 from matplotlib import pyplot
+import sys
+
 
 # load an image
 def load_image(filename, size=(256,256)):
 	# load image with the preferred size
 	pixels = load_img(filename, target_size=size)
+
 	# convert to numpy array
 	pixels = img_to_array(pixels)
+	print(pixels)
+	print(pixels.shape)
 	# scale from [0,255] to [-1,1]
 	pixels = (pixels - 127.5) / 127.5
 	# reshape to 1 sample
 	pixels = expand_dims(pixels, 0)
+	print(pixels)
+	print(pixels.shape)
 	return pixels
 
 # load source image
---
+++ Project/code/test_flask.py
@@ -0,0 +1,53 @@
+import flask
+from flask import Flask, request, render_template, jsonify
+from tensorflow.keras.models import load_model
+import numpy as np
+import imageio
+import sys
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from numpy import load
+from numpy import expand_dims
+import matplotlib.pyplot as plt
+import cv2
+import pandas as pd
+
+
+app=Flask(__name__)
+
+
+# 메인페이지 라우팅
+@app.route("/")
+@app.route("/index")
+def index():
+    return flask.render_template("index.html")
+
+# 데이터 예측 처리
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method=='POST' :
+
+        file=request.files['img']
+        if not file : return render_template('index.html', label="NoFiles")
+
+        img=imageio.imread(file)
+        img=cv2.resize(img, (256, 256))
+        img=img_to_array(img)
+        img = (img - 127.5) / 127.5
+        img = expand_dims(img, 0)
+        print(img.shape)
+
+        model = load_model('model_002760.h5')
+        predict=model.predict(img)
+
+        predict = (predict + 1) / 2.0
+        # plot the image
+        plt.imshow(predict[0])
+        plt.axis('off')
+        plt.savefig('./code/static/out.png')
+
+    return "<img src='{{ url_for('static', out.png) }}'>"
+
+
+if __name__ == '__main__' :
+    app.run(host="127.0.0.1", port="8080")
